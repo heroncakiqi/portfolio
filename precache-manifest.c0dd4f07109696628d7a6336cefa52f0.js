b5e5962d730deb329268dbf9c889ab954033ff19
Heron <cakiqiheron@gmail.com>
2019-12-19 19:53:30 +0100

    Updates

---
+++ precache-manifest.c0dd4f07109696628d7a6336cefa52f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d616d1292e1824b3ac9941323872389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae97d4e07fa54f01f9d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "befc16f737b15a6ceb5e",
+    "url": "/static/js/2.bb33a59f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bb33a59f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bae97d4e07fa54f01f9d",
+    "url": "/static/js/main.bdfb9011.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
