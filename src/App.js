43ace5f6481cac01fc4655b81dbdbf48d79e1daf
Heron <cakiqiheron@gmail.com>
2019-12-19 20:25:51 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <p>
           <code>heron cakiqi</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
