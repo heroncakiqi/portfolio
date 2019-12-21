fc4cf97df02b7a9ef175b4b33b924d20bc96d825
Heron <cakiqiheron@gmail.com>
2019-12-21 19:34:44 +0100

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Nav from './Nav';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState('');
+  const text = `heron cakiqi \n full stack developer`
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const letterToAdd = state.length
+      setState(`${state + text[letterToAdd]}`);
+    }, 50);
+    if(state.length === text.length) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [state, text]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <code>heron cakiqi</code>
-          <br/>
-          <code>full stack developer</code>
-        </p>
+        <div>
+          <h1>{state}<span id="blinker">_</span></h1>
+        </div>
       </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav>
+        <div>about</div>
+        <div>projects</div>
+        <div>contact</div>
+    </nav>
+  )
+}
