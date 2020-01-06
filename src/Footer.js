ed287797b8002e3244d13b97365bd5e978b4f82f
Heron <cakiqiheron@gmail.com>
2020-01-06 22:51:55 +0100

    added footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components'; 
+import { FaGithub, FaCodepen } from 'react-icons/fa';
+import { AiOutlineProfile, AiOutlineMail } from "react-icons/ai";
+
+const FooterStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  .icons-group {
+    margin: 2rem 0;
+    svg {
+      margin: 1rem;
+    }
+  }
+`;
 
 function Footer() {
   return (
-      <h2></h2>
+      <FooterStyles>
+        <div className="icons-group">
+          <FaGithub />
+          <AiOutlineMail />
+          <FaCodepen />
+          <AiOutlineProfile />
+        </div>
+      </FooterStyles>
   )
 }
 
